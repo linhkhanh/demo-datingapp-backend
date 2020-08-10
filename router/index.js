d39ad9783fc89e8f2ddb77858bc54d413a5a1094
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-10 17:11:20 +0800

    add getall, get oneById, create, update

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 const usersController = require('../controllers/usersController');
+const messageController = require('../controllers/messageController');
 const sessionController = require('../controllers/session');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
@@ -18,20 +19,34 @@ module.exports = app => {
     
     // check Authentication
     app.get('/check_authentication', sessionController.checkAuthentication);
-
     
+    // get ALL CHAT ROOMS
+    app.get('/chat_room', messageController.getAll);
 
     app.get('/:id', usersController.getById);
+
+    //GET ONE CHAT ROOM
+    app.get('/chat_room/:id_chat_room', messageController.getById);
+
     app.post('/', usersController.create);
 
+    // log in with fb
     app.post('/get_data_fb', sessionController.getDataFacebook);
     app.post('/log_in_with_fb', sessionController.logInWithFacebookSubmit);
 
+     // upload avata
+     app.post('/avatar-upload', upload.single('file'), usersController.uploadAvatar);
+
+    // create chat room
+    app.post('/chat_room', messageController.createChatRoom);
     app.put('/:id', usersController.updateById);
+
+    // add new message to a chat room 
+    app.put('/chat_room/:id_chat_room', messageController.updateById);
+
     app.delete('/:id', usersController.deleteById);
 
-    // upload avata
-    app.post('/avatar-upload', upload.single('file'), usersController.uploadAvatar);
+   
     // login-submit
     app.post('/login_submit', sessionController.loginSubmit);
 
